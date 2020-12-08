b473992ce061bf4dfddced32065a0da689065a4a
Mr.Cheng <mkt_aoo96@hotmail.com>
2020-12-08 16:20:13 +0000

    Add SplashScreen

--- app/src/main/java/com/chengbo/frontlimit/MainActivity.java
+++ app/src/main/java/com/chengbo/frontlimit/MainActivity.java
@@ -2,7 +2,9 @@ package com.chengbo.frontlimit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
